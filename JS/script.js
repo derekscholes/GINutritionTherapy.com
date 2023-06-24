f4a56d508aed76516d976da98e11460c0c5b173d
Derek J. Scholes <132800749+derekscholes@users.noreply.github.com>
2023-06-24 23:41:31 +0000

    updates to a lot

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,7 @@
-
 var state=false;
   function expand(){
     if(state==false){
-      document.getElementById('items').style.transform='scaleX(1)';  
+      document.getElementById('items').style.transform='scaleX(1)';
         state=true;
   }
     else{
@@ -10,4 +9,20 @@ var state=false;
       document.getElementById('toggle1').style.transform='rotate(0deg)';
 	state=false;
   }
+}
+
+//Blog Page-->//
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
 }
